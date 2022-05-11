4dfa09fedbde82dda348dbc33e6fb774917b85c8
David <davidmartinezmit@gmail.com>
2022-05-11 17:49:52 +0200

    Register +  Login + Events casi

--- DAO/events.dao.js
+++ DAO/events.dao.js
@@ -4,7 +4,7 @@ class EventsDAO {
 
     async postEvent(event, owner_id) {  //añadir un nuevo event
 
-        const results = await global.connection.promise().query(`INSERT INTO ?? (name, owner_id, date, image, location, description, eventStart_date, eventEnd_date, n_participators, type) VALUES ("${event.name}", ${owner_id}, "${Date.now()}", "${event.image}", "${event.location}", "${event.description}", "${event.eventStart_date}", "${event.eventEnd_date}", ${event.n_participators}, "${event.type},")`, [this.tabla]);
+        const results = await global.connection.promise().query(`INSERT INTO ?? (name, owner_id, date, image, location, description, eventStart_date, eventEnd_date, n_participators, type) VALUES ("${event.name}", ${owner_id}, "${Date.now()}", "${event.image}", "${event.location}", "${event.description}", "${event.eventStart_date}", "${event.eventEnd_date}", ${event.n_participators}, "${event.type},")`, [tabla]);
         console.log(results);
         return results;
     }
