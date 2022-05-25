1c73910f4af65cd513d021d845612bad410be080
superfer2002 <fporcel2002@gmail.com>
2022-05-25 18:15:53 +0200

    messages finished

--- DAO/messages.dao.js
+++ DAO/messages.dao.js
@@ -5,10 +5,24 @@ const moment = require('moment');
 class MessagesDAO {
 
     async postMessage(message) {  //añadir un nuevo message
-        //${moment().format()}
-        const results = await global.connection.promise().query(`INSERT INTO ?? (idUser, idPost, idComent, contenido) VALUES (${comment.idUser}, ${comment.idPost}, ${comment.idComment}, "${comment.contenido}")`, [this.tabla]);
-        return results;
+    
+        const add = await global.connection.promise().query(`INSERT INTO ?? (content, user_id_send, user_id_recived, timeStamp) VALUES ("${message.content}", ${message.user_id_send}, ${message.user_id_recived}, "${moment().format()}")`, [tabla]);
+        const results = await global.connection.promise().query(`SELECT id, content, user_id_send, user_id_recived, timeStamp FROM ?? WHERE id = "${add[0].insertId}"`, [tabla]);
+        return results[0];
     }
+
+    async getMessagesUser(id) {  
+
+        const results = await global.connection.promise().query(`SELECT users.id, users.name, users.last_name, users.email FROM users INNER JOIN message ON users.id = message.user_id_send WHERE message.user_id_recived = ?`, [id]);
+        return results[0];
+    }
+
+    async getMessagesUserId(id, owner_id) {  
+
+        const results = await global.connection.promise().query(`SELECT * FROM ?? WHERE (user_id_send = ${owner_id} AND user_id_recived = ${id}) OR (user_id_send = ${id} AND user_id_recived = ${owner_id})`, [tabla]);
+        return results[0];
+    }
+
 }
 
 module.exports = MessagesDAO;
--- routes/messages.route.js
+++ routes/messages.route.js
@@ -5,8 +5,19 @@ const privateRoute = require("../Middleware/private.mid");
 const MessagesDAO = require("../DAO/messages.dao");
 const mdao = new MessagesDAO();
 
-router.get("/", async (req, res, next) => {  //Consultar todos los posts
+router.post("/", privateRoute, async (req, res, next) => { 
     res.json(await mdao.postMessage(req.body));
 })
+
+router.get("/users", privateRoute, async (req, res, next) => {
+    const owner_id = req.USER_ID;
+    res.json(await mdao.getMessagesUser(owner_id));
+})
+
+router.get("/:id", privateRoute, async (req, res, next) => {
+    const owner_id = req.USER_ID;
+    res.json(await mdao.getMessagesUserId(req.params.id, owner_id));
+})
+
 
 module.exports = router;
