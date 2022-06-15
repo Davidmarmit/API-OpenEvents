376cb9224ead14a344534ef62969c1fd0a9ac97c
David <davidmartinezmit@gmail.com>
2022-06-15 23:42:15 +0200

    Events ok 2

--- DAO/events.dao.js
+++ DAO/events.dao.js
@@ -10,7 +10,7 @@ class EventsDAO {
 
             const add = await global.connection.promise().query(`INSERT INTO ?? (name, owner_id, date, image, location, description, eventStart_date, eventEnd_date, n_participators, type) VALUES ("${event.name}", ${owner_id}, "${moment().format()}", "${event.image}", "${event.location}", "${event.description}", "${event.eventStart_date}", "${event.eventEnd_date}", ${event.n_participators}, "${event.type}")`, [tabla]);
             const [results] = await global.connection.promise().query(`SELECT name, image, location, description, eventStart_date, eventEnd_date, n_participators, type, owner_id, date FROM ?? WHERE id = "${add[0].insertId}"`, [tabla]);
-            return results;
+            return results[0];
         
         } catch (error) {
             return { error: "Missing required parameters." };
